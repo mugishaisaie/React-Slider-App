bf801397ef459dc166a1bbf936a228ef6e5423f2
mugisha isaie <mugishaisaieog1@gmail.com>
2024-08-28 20:22:15 +0200

    First commit afer writting slider code project

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+
+import './App.css'
+import Carousel from './Carousel'
+import SlickCoursel from './SlickCoursel'
+
+function App() {
+
+  return (
+    <div>
+      
+   {/* <Carousel /> */}
+   <SlickCoursel />
+    </div>
+  )
+ 
+}
+
+export default App
